2e281438d7f9c231923213f33435b76eef9748b2
SirisNeupane <58123410+SirisNeupane@users.noreply.github.com>
2022-10-15 17:49:45 -0400

    create_deck

--- main.py
+++ main.py
@@ -25,3 +25,19 @@ class Deck:
     def get_card(self):
         card = self.deck.pop(0)
         return card
+
+        def __str__(self):
+            return_string = ""
+            for card in self.deck:
+                return_string += card.__str__() + "-- "
+            return return_string
+
+        def __repr__(self):
+            return self.deck
+
+def create_deck():
+    deck = [Card(suit, value) for value in
+            ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'] for suit in
+            ['Hearts', 'Spades', 'Diamonds', 'Clubs']]
+    random.shuffle(deck);
+    return deck
