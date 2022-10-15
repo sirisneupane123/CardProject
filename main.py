cbe2f0c47161ddc71b21b06e25b8e9f91abc7616
SirisNeupane <58123410+SirisNeupane@users.noreply.github.com>
2022-10-15 17:48:00 -0400

    Deck class

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ class Card():
 
     def __repr__(self):
         return self.val + ' of ' + self.suit
+
+
+class Deck:
+
+    def __init__(self):
+        self.deck = []
+
+        values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+        for suit in ['Hearts', 'Spades', 'Diamonds', 'Clubs']:
+            for value in values:
+                card = Card(suit, value)
+                self.deck.append(card)
